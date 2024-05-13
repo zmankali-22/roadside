fff028cc4e3455004ef61cc0bafe58c81415d986
utsav <sav.acharya22@gmail.com>
2024-05-13 21:33:50 +1000

    capital using map

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,9 @@
+let students = [
+    {name: 'John', rollNumber: 31, marks: 80},
+    {name: 'Peter', rollNumber: 32, marks: 85},
+    {name: 'Thomas', rollNumber: 33, marks: 90},
+    {name: 'Mark', rollNumber: 34, marks: 70},
+]
 
-let x =3
-console.log(x)
-nums = [1,2,3]
-console.log( nums.map(num => (num*3)))
-
-console.log("abe oye")
+let capitalStudents = students.map((student) => student.name.toUpperCase())
+console.log(capitalStudents)
