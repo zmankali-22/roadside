eb920571e8a13324ead23786196ac3280355d2ae
utsav <sav.acharya22@gmail.com>
2024-05-14 12:51:22 +1000

    map filter reduce

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,40 @@
 let students = [
-    {name: 'John', rollNumber: 31, marks: 80},
-    {name: 'Peter', rollNumber: 32, marks: 85},
+    {name: 'John', rollNumber: 12, marks: 30},
+    {name: 'Peter', rollNumber: 10, marks: 55},
     {name: 'Thomas', rollNumber: 33, marks: 90},
     {name: 'Mark', rollNumber: 34, marks: 70},
+    {name: 'Jamie', rollNumber: 30, marks:45}
 ]
 
-let capitalStudents = students.map((student) => student.name.toUpperCase())
-console.log(capitalStudents)
+// let capitalStudents = students.filter((student) => student.marks > 60 && student.rollNumber > 15)
+// console.log(capitalStudents)
+
+// let sumOfMarks = students.reduce((acc, curr) => (acc + curr.marks), 0) 
+
+// console.log(sumOfMarks)
+// let namesMore60 = students.filter(student => student.marks > 60).map(student => student.name)
+
+// console.log(namesMore60)
+
+
+let totalMarls = students.map(student => {
+    if (student.marks < 60) {
+        student.marks += 20
+    }
+    return student
+}).filter(student => student.marks > 60).reduce((acc, curr) => (acc + curr.marks), 0)
+
+
+console.log(totalMarls)
+
+
+let totalMarks = students.forEach(student => {
+    if (student.marks < 60) {
+        student.marks += 20
+    }
+    return student
+})
+
+console.log(totalMarks)
+console.log(students)
+
