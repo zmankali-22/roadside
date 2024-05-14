df73d22805d487feec3f1888f305e1bcad121b41
utsav <sav.acharya22@gmail.com>
2024-05-14 16:52:37 +1000

    private counter

--- javascript/scriptfunc.js
+++ javascript/scriptfunc.js
@@ -1,69 +1,25 @@
-// // The following variables are defined in the global scope
-// const num1 = 20;
-// const num2 = 3;
-// const name = "Chamakh";
+function counter() {
+    var _counter = 0
 
-// // This function is defined in the global scope
-// function multiply() {
-//   return num1 * num2;
-// }
+    function add(increment) {
+        _counter += increment
+        
+    }
 
-// console.log(multiply()); // 60
+    function retrieve() {
+        return "Counter = " + _counter
+    }
 
-// // A nested function example
-// function getScore() {
-//   const num1 = 2;
-//   const num2 = 3;
-
-//   function add() {
-//     return `${name} scored ${num1 + num2}`;
-//   }
-
-//   return add();
-// }
-
-// console.log(getScore()); // "Chamakh scored 5"
-
-
-// for (let i =0 ; i < 5 ; i++) {
-//     setTimeout(function(){
-//         console.log(i)
-//     }, i * 1000);
-// }
-
-
-// for (var i =0 ; i < 5 ; i++) {
-//     setTimeout(function(){
-//         console.log(i)
-//     }, i * 1000);
-// }
-
-
-// var x = 21
-
-
-// var func = function () {
-//     console.log(x)
-//     var x = 100
-// }
-
-// func()
-
-
-// const fn = (a,  x, y, ...numbers) => {
-//     console.log(x, y, numbers)
-// }
-
-// fn(2, 5, 3 ,7, 7, 9, 5)
-
-function func() {
-    console.log(arguments)
+    return {
+        add,
+        retrieve
+    }
 }
 
-func(2, 3, 5)
+const c = counter()
 
-const func1 = function () {
-    console.log(arguments)
-}
+c.add(1)
+c.add(2)
+c.add(3)
 
-func1(2, 3, 5)
+console.log(c.retrieve())
