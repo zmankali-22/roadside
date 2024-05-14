23dd829ec1ab1f6c9f849f40983a3cc930192a45
utsav <sav.acharya22@gmail.com>
2024-05-15 09:48:51 +1000

    class baed comp

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,42 @@
 //  list of product in an array of object
 
 const products = [
-    {id: 1, name: 'Product A', price: 20, category: 'Electronics'},
-    {id: 2, name: 'Product B', price: 30, category: 'Clothing'},
-    {id: 3, name: 'Product C', price: 40, category: 'Electronics'},
-    {id: 4, name: 'Product D', price: 50, category: 'Clothing'},
-    {id: 5, name: 'Product E', price: 60, category: 'Electronics'},
-    {id: 6, name: 'Product F', price: 70, category: 'Electronics'},
+  { id: 1, name: "Product A", price: 20, category: "Electronics" },
+  { id: 2, name: "Product B", price: 30, category: "Clothing" },
+  { id: 3, name: "Product C", price: 40, category: "Electronics" },
+  { id: 4, name: "Product D", price: 50, category: "Clothing" },
+  { id: 5, name: "Product E", price: 60, category: "Electronics" },
+  { id: 6, name: "Product F", price: 70, category: "Electronics" },
+];
 
-]
-
+const names = ["alice", "fred", "charlie", "fred"];
+const newNames = [...new Set(names)];
 
 function App() {
+  return <div>
 
+    <Counter />
+  </div>;
+}
 
-  return (
-    <div>
-      <h1>Counter</h1>
-        <ul>
-            {products.map(product => (
-                <li key ={product.id} >
-                   {product.name} - ${product.price} - {product.category}
-                </li>
-            ))}
-        </ul>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+  }
+  incrementCount = () =>
+    this.setState({ count: this.state.count + 1 });
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
