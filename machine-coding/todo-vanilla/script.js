7813831562417c530609a218ac4698ac03b87c49
utsav <sav.acharya22@gmail.com>
2024-05-19 11:43:47 +1000

    vanilla todo app

---
+++ machine-coding/todo-vanilla/script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const todoForm = document.querySelector(".todo-form");
+  const todoInput = document.querySelector(".todo-input");
+  const todoSubmit = document.querySelector(".todo-submit");
+  const todoList = document.querySelector(".todo-list");
+  let editMode = false;
+  let editItem = null;
+
+  todoForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const todoText = todoInput.value.trim();
+
+    if (todoText !== "") {
+      if (editMode) {
+        editItem.firstChild.textContent = todoText;
+        todoSubmit.innerText = "Add Todo";
+        editMode = false;
+        editItem = null;
+      } else {
+        // add todos
+        addTodoItem(todoText);
+       
+      }
+    } else {
+      alert("Please enter a valid task");
+    }
+  });
+
+  todoList.addEventListener("click", function (event) {
+    const target = event.target;
+    if (target.tagName === "BUTTON") {
+      const todoItem = target.parentNode;
+      if (target.innerText === "❌") {
+        todoItem.remove();
+      } else if (target.innerText === "🖊") {
+        editMode = true;
+        editItem = todoItem;
+        todoSubmit.innerText = "Edit Todo";
+        todoInput.value = todoItem.firstChild.textContent;
+        todoInput.focus();
+      }
+    }
+  });
+
+  function addTodoItem(todoText) {
+    const todoItem = document.createElement("li");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    todoItem.innerHTML = `<span>${todoText}</span>`;
+    editButton.innerText = "🖊";
+    deleteButton.innerText = "❌";
+
+    todoItem.appendChild(editButton);
+    todoItem.appendChild(deleteButton);
+    todoList.appendChild(todoItem);
+  }
+});
