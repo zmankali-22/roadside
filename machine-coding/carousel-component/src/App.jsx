9397379758ad5e9b8ab739fbd40bc36c32f4641e
utsav <sav.acharya22@gmail.com>
2024-05-23 14:26:15 +1000

    current index logic

--- machine-coding/carousel-component/src/App.jsx
+++ machine-coding/carousel-component/src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Carousel from "./components/Carousel";
 
 function App() {
   const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState([]);
+  console.log(images)
+
+  // https://jsonplaceholder.typicode.com/photos?_limit=8
+
+  const fetchImages = async (imgLimit) => {
+    setLoading(true)
+    try{
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${imgLimit}`)
+      const data = await response.json()
+      setImages(data)
+
+    } catch (error) {
+      console.group("Error fetching data:", error)
+
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchImages(8)
+  }, [])
 
   return (
-    <>
+    <div className="carousel-container">
       <Carousel 
-      images={[]} 
+      images={images} 
       isLoading={loading}
-      // imagesPerSlide ={}
+      // imagePerSlide ={}
       // imageLimit ={}
       // customPrevBUtton ={}
       // customNextButton 
       />
-    </>
+    </div>
   );
 }
 
--- machine-coding/carousel-component/src/components/Carousel.jsx
+++ machine-coding/carousel-component/src/components/Carousel.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-export default function Carousel() {
-  return (
-    <div>Carousel</div>
-  )
+/* eslint-disable react/prop-types */
+export default function Carousel({
+  images = [],
+  isLoading = false,
+  imageLimit = images.length,
+  customPrevButton,
+  customNextButton,
+  imagePerSlide = 1,
+}) {
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+  console.log(currentIndex)
+
+  useEffect(() => {
+    if (images.length > 0) {
+      setCurrentIndex(0)
+    }
+  },[images])
+
+  const goPrev = () => {
+    setCurrentIndex(prevIndex => prevIndex === 0 ? images.length -1 : prevIndex - 1)
+  }
+
+  const goNext = () => {
+    setCurrentIndex(prevIndex => prevIndex === images.length -1 ? 0 : prevIndex + 1 )
+  }
+
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="carousel">
+      <div className="image-container">
+        {images
+          .slice(
+            0,
+            imageLimit > images.length ? images.length : imageLimit
+          )
+          .map((image) => (
+            <img
+              key={image.id}
+              src={image.url}
+              alt={image.title}
+              className="image"
+            />
+          ))}
+      </div>
+      <button className="btn prev" onClick={goPrev}>Prev</button>
+      <button className="btn next" onClick={goNext}>Next</button>
+    </div>
+  );
 }
