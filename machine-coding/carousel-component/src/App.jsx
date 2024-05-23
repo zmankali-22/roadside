00dc6227571d91f6cca0760c3039b8fb970ee50f
utsav <sav.acharya22@gmail.com>
2024-05-23 20:36:14 +1000

    carousel component

--- machine-coding/carousel-component/src/App.jsx
+++ machine-coding/carousel-component/src/App.jsx
@@ -5,38 +5,56 @@ import Carousel from "./components/Carousel";
 function App() {
   const [loading, setLoading] = useState(false);
   const [images, setImages] = useState([]);
-  console.log(images)
 
   // https://jsonplaceholder.typicode.com/photos?_limit=8
 
   const fetchImages = async (imgLimit) => {
-    setLoading(true)
-    try{
-      const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${imgLimit}`)
-      const data = await response.json()
-      setImages(data)
-
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?_limit=${imgLimit}`
+      );
+      const data = await response.json();
+      setImages(data);
     } catch (error) {
-      console.group("Error fetching data:", error)
-
+      console.group("Error fetching data:", error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchImages(8)
-  }, [])
+    fetchImages(8);
+  }, []);
 
   return (
     <div className="carousel-container">
-      <Carousel 
-      images={images} 
-      isLoading={loading}
-      // imagePerSlide ={}
-      // imageLimit ={}
-      // customPrevBUtton ={}
-      // customNextButton 
+      <Carousel
+        images={images}
+        isLoading={loading}
+        imagePerSlide={2}
+        imageLimit={4}
+        onImgClick={(image, index) => {
+          console.log("Image clicked:", image, "at index:", index);
+        }}
+        customPrevButton={(onClick) => (
+          <button
+            className="btn prev"
+            style={{ background: "red" }}
+            onClick={onClick}
+          >
+            Prev
+          </button>
+        )}
+        customNextButton={(onClick) => (
+          <button
+            className="btn next"
+            style={{ background: "blue" }}
+            onClick={onClick}
+          >
+            Next
+          </button>
+        )}
       />
     </div>
   );
--- machine-coding/carousel-component/src/components/Carousel.jsx
+++ machine-coding/carousel-component/src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 /* eslint-disable react/prop-types */
 export default function Carousel({
@@ -7,47 +7,80 @@ export default function Carousel({
   imageLimit = images.length,
   customPrevButton,
   customNextButton,
-  imagePerSlide = 1,
+  onImgClick = () => {},
+  imagePerSlide = 2,
 }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageWidth, setImageWidth] = useState(0);
 
-  const [currentIndex, setCurrentIndex] = useState(0)
-  console.log(currentIndex)
+  const imageRef = useRef();
 
   useEffect(() => {
     if (images.length > 0) {
-      setCurrentIndex(0)
+      setCurrentIndex(0);
     }
-  },[images])
+  }, [images]);
 
   const goPrev = () => {
-    setCurrentIndex(prevIndex => prevIndex === 0 ? images.length -1 : prevIndex - 1)
-  }
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? imageLimit - 1 : prevIndex - 1
+    );
+  };
 
   const goNext = () => {
-    setCurrentIndex(prevIndex => prevIndex === images.length -1 ? 0 : prevIndex + 1 )
-  }
+    setCurrentIndex((prevIndex) =>
+      prevIndex === imageLimit - 1  ? 0 : prevIndex + 1
+    );
+  };
+
+  console.log(imageRef?.current?.offsetWidth);
 
   return isLoading ? (
     <div>Loading...</div>
   ) : (
-    <div className="carousel">
-      <div className="image-container">
+    <div
+      className="carousel"
+      style={{ width: imagePerSlide * imageWidth }}
+    >
+      <div
+        className="image-container"
+        style={{
+          transform: `translateX(-${currentIndex * imageWidth}px)`,
+        }}
+      >
         {images
           .slice(
             0,
             imageLimit > images.length ? images.length : imageLimit
           )
-          .map((image) => (
+          .map((image, index) => (
             <img
+              onLoad={() =>
+                setImageWidth(imageRef?.current?.offsetWidth)
+              }
+              ref={imageRef}
               key={image.id}
               src={image.url}
               alt={image.title}
+              onClick={() => onImgClick(image, index)}
               className="image"
             />
           ))}
       </div>
-      <button className="btn prev" onClick={goPrev}>Prev</button>
-      <button className="btn next" onClick={goNext}>Next</button>
+      {customPrevButton instanceof Function ? (
+        customPrevButton(goPrev)
+      ) : (
+        <button className="btn prev" onClick={goPrev}>
+          Prev
+        </button>
+      )}
+      {customNextButton instanceof Function ? (
+        customNextButton(goNext)
+      ) : (
+        <button className="btn next" onClick={goNext}>
+          Next
+        </button>
+      )}
     </div>
   );
 }
